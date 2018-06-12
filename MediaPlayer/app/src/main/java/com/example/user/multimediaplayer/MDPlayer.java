02be63fdebc699dbf8fdcbde4c05819132d0dec0
Alexander Urriola <alexx198906@gmail.com>
2018-06-12 01:01:39 -0500

    correcciones de musica

--- MediaPlayer/app/src/main/java/com/example/user/multimediaplayer/MDPlayer.java
+++ MediaPlayer/app/src/main/java/com/example/user/multimediaplayer/MDPlayer.java
@@ -4,9 +4,10 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.ActionBar;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -14,10 +15,11 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
+    private String TAG = "MDPlayer";
     static MediaPlayer np;
     ArrayList<File> cns;
     ArrayList<File> nombrecns;
@@ -29,12 +31,15 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
     ImageButton btnpy, btnfw, btnbw, btnbck;
     TextView nombrecancion,tTranscurrido, tRestante;
     SeekBar sb;
+    private Handler myHandler = new Handler();
+    private double startTime = 0;
+    private double finalTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mdplayer);
-        np= new MediaPlayer();
+//        np= new MediaPlayer();
         // captura de accion de ImageButton
         btnpy = (ImageButton) findViewById(R.id.btnimgPlay);
         btnfw = (ImageButton) findViewById(R.id.btnimgForward);
@@ -59,63 +64,7 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
 
 
         //logica
-
-        actseekbar = new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                int trtime = np.getDuration();
-                sb.setMax(trtime);
-                int posact = 0;
-                int ejec = 0;
-                boolean bool = false;
-                while (posact < trtime) {
-                    try {
-                        sleep(500);
-                        posact = np.getCurrentPosition();
-                        sb.setProgress(posact);
-
-                        ejec = sb.getProgress();
-                        aux = getHRM(ejec);
-                        tRestante.setText(aux.toString().trim());
-                    } catch (Exception e) {
-                        tRestante.setText(aux);
-                    }
-                }
-            }
-        };
-        if (np != null) {
-            np.stop();
-        }
-        try {
-            Intent i = getIntent();
-            Bundle b = i.getExtras();
-            cns = (ArrayList) b.getParcelableArrayList("songs");
-            posicion = (int) b.getInt("pos", 0);
-            uri = Uri.parse(cns.get(posicion).toString());
-            nombrecancion.setText(cns.get(posicion).getName().toString());
-            np = MediaPlayer.create(getApplication(), uri);
-            actseekbar.start();
-            np.start();
-            tRestante.setText(getHRM(np.getDuration()));
-        } catch (Exception e) {
-        }
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                np.seekTo(seekBar.getProgress());
-            }
-        });
+        playMusic();
     }
 
     private String getHRM(int milliseconds) {
@@ -132,23 +81,28 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View v) {
 
         int id = v.getId();
+
         switch (id) {
             case R.id.btnimgPlay:
-                System.out.println("IMPRIMIENDO");
-                System.out.println(np.isPlaying());
+                Log.d(TAG,"IMPRIMIENDO");
+               Log.d(TAG,""+np.isPlaying());
                 if (np.isPlaying()) {
                     btnpy.setImageResource(R.drawable.play);
 
                     np.pause();
                 } else {
+
                     btnpy.setImageResource(R.drawable.pause);
-                    try{np.prepare();
+                    try{
+                        np.prepare();
                         np.start();
-                    }catch (IllegalStateException e){} catch (IOException e) {
+                    }catch (IllegalStateException e){
+                        e.printStackTrace();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
 
-                    System.out.println(np.isPlaying());
+                    Log.d(TAG,""+np.isPlaying());
                 }
                 break;
             /*case R.id.btnimgForward:
@@ -174,7 +128,7 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
         //mp.release();
 
         posicion = (posicion + 1) % cns.size();
-        nombrecancion.setText(cns.get(posicion).getName().toString());
+        nombrecancion.setText(cns.get(posicion).getName());
 
         uri = Uri.parse(cns.get(posicion).toString());
         np = MediaPlayer.create(getApplicationContext(), uri);
@@ -185,7 +139,7 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
         try{
             sb.setMax(np.getDuration());//le envia el maximo a so portar seebark ok
         }catch (Exception e){
-
+            e.printStackTrace();
         }
     }
 
@@ -198,13 +152,90 @@ public class MDPlayer extends AppCompatActivity implements View.OnClickListener{
         } else {
             posicion = posicion - 1;
         }
-        nombrecancion.setText(cns.get(posicion).getName().toString());
+        nombrecancion.setText(cns.get(posicion).getName());
         uri = Uri.parse(cns.get(posicion).toString());
         np = MediaPlayer.create(getApplicationContext(), uri);
         np.start();
         sb.setMax(0);//le envia el maximo a so portar seebark ok
         tTranscurrido.setText( getHRM(np.getDuration()));//mostrar el tiempo que dura la cancion
         sb.setMax(np.getDuration());
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    public void playMusic()
+    {
+
+        if (np != null) {
+            np.stop();
+        }
+        try {
+            Intent i = getIntent();
+            Bundle b = i.getExtras();
+            cns = (ArrayList) b.getParcelableArrayList("Canciones");
+            posicion = (int) b.getInt("pos", 0);
+            uri = Uri.parse(cns.get(posicion).toString());
+            nombrecancion.setText(cns.get(posicion).getName());
+            np = MediaPlayer.create(getApplication(), uri);
+            np.start();
+            tRestante.setText(getHRM(np.getDuration()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                np.seekTo(seekBar.getProgress());
+            }
+        });
+
+
+        finalTime = np.getDuration();
+        startTime = np.getCurrentPosition();
+        sb.setMax(np.getDuration());
+        sb.setProgress((int)startTime);
+
+
+        tRestante.setText(String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                finalTime)))
+        );
+
+        tTranscurrido.setText(String.format("%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                startTime)))
+        );
+
+
+        Runnable UpdateSongTime = new Runnable() {
+            public void run() {
+                startTime = np.getCurrentPosition();
+                Log.d(TAG,"getCurrentPosition(): "+startTime);
+                tTranscurrido.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                        toMinutes((long) startTime)))
+                );
+                sb.setProgress((int)startTime);
+                myHandler.postDelayed(this, 100);
+            }
+        };
+        myHandler.postDelayed(UpdateSongTime,100);
     }
 
 }
